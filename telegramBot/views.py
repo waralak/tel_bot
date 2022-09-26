ce20c31ba71a83156f358d7aee10ae0e935d19d0
asdf <asdf@asdfs-MacBook-Pro.local>
2022-09-26 23:30:49 +0900

    Next Commit22

--- telegramBot/views.py
+++ telegramBot/views.py
@@ -3,18 +3,20 @@ from django.http import HttpResponse
 # Create your views here.
 import telegram
 import os
+import sys
 
 bot = telegram.Bot(token=os.environ.get("TOKEN", ""))
 
 def index(request):
     if request.method == "GET":
         print("YES, it is GETTTTT")
-
+        sys.stdout.flush()
     update = telegram.Update.de_json(request.POST, bot)
     
     chat_id = update.message.chat.id
     msg_id = update.message.message_id
 
     print("Message Successfully Received from {}.{}".format(chat_id, msg_id))
+
 
     return HttpResponse("")
